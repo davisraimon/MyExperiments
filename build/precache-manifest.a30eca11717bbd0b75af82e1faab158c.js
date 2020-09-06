7cce3fba71e67943e4493471cd91ba84032d095f
Davis Raimon <davisraimon@gmail.com>
2020-09-06 20:49:33 +0530

    serving static files commit

---
+++ build/precache-manifest.a30eca11717bbd0b75af82e1faab158c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1576fc2d0a06edf0647ea6cf8a566c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94f365504ead4af652c4",
+    "url": "/static/css/main.e2b0825e.chunk.css"
+  },
+  {
+    "revision": "ed18c01d576516663318",
+    "url": "/static/js/2.0cd3f47f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0cd3f47f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94f365504ead4af652c4",
+    "url": "/static/js/main.debeaac0.chunk.js"
+  },
+  {
+    "revision": "81356918c65a137c977e",
+    "url": "/static/js/runtime-main.cf942600.js"
+  }
+]);
